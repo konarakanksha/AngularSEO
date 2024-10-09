e5ed0995b1eb9f825108b221ad754fe3a7c0e360
Akanksha Konar <akanksha.konar@qbent.in>
2024-10-09 12:12:32 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularSEO';
+
+  constructor(private metaService: Meta, private titleService: Title) {
+    // Set page title
+    this.titleService.setTitle('Home - ANGULARSEO');
+
+    // Set meta tags for this specific page
+    this.metaService.addTags([
+      { name: 'description', content: 'This is the homepage of ANGULARSEO, providing a great user experience.' },
+      { name: 'keywords', content: 'angular, seo, santi gopal kiritee, ANGULARSEO,angular, seo,' },
+      { name: 'robots', content: 'index, follow' },
+      { property: 'og:title', content: 'Home - ANGULARSEO' },
+      { property: 'og:description', content: 'This is the App page.' },
+      { property: 'og:url', content: 'https://konarakanksha.github.io/AngularSEO/' }
+    ]);
+  }
 }
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-users',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
-  constructor() { }
+  constructor(private metaService: Meta, private titleService: Title) { }
 
   ngOnInit() {
+    // Set page title
+    this.titleService.setTitle('Home - ANGULARSEO');
+
+    // Set meta tags for this specific page
+    this.metaService.addTags([
+      { name: 'description', content: 'This is the homepage of Your App Name, providing a great user experience.' },
+      { name: 'keywords', content: 'angular, seo, santi gopal kiritee, ANGULARSEO' },
+      { name: 'robots', content: 'index, follow' },
+      { property: 'og:title', content: 'Home - ANGULARSEO' },
+      { property: 'og:description', content: 'This is the user page.' },
+      { property: 'og:url', content: 'https://konarakanksha.github.io/AngularSEO/' }
+    ]);
   }
 
 }
